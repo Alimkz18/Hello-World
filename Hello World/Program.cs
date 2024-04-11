2af2d0f3821d8b2a637035410d4b0395ed183a5c
zhain <zhain@NIGGER>
2024-04-11 00:13:17 -0500

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Hello, World!");
+
+
+            ReadKey();
+
+
+
+        }
+
+    }
+}
